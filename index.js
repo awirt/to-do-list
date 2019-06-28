cd3cfa7a21611694d5cd4b00b7ce91f91599e5ed
Andrew Wirt <15827151+awirt@users.noreply.github.com>
2019-06-28 17:41:40 -0500

    Add event listener and handler for submit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//Prove JS File is Linked to HTML
 console.log("JS is loaded.");
+
+//Variables
+var submitBtn = $(".submit");
+var appendContainer = $(".append-container");
+
+
+//Functions
+function addItem(event) {
+  event.preventDefault();
+  var userText = $(".text").val();
+  appendContainer.append(`
+    <p>#{userText}</p>
+  `);
+}
+
+//Calling Functions & Event Listeners
+  //1. when submit button is clicked, append user input text to the container
+submitBtn.on("click", addItem);
